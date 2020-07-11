45fd60a3acc47e653ec988856b7ead74432dc256
Krepy <neadiatre@gmail.com>
2020-07-11 13:30:16 +0300

    Changed to generic view

--- todolist/todo/urls.py
+++ todolist/todo/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'todo'
 urlpatterns = [
-    path('', views.IndexView, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('today', views.TodayView.as_view(), name='today'),
+    path('week', views.WeekView.as_view(), name='week'),
+    path('expired', views.ExpiredView.as_view(), name='expired'),
+    path('add', views.AddItemView, name='add'),
 ]
--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -5,19 +5,45 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
+from .models import Note
 # Create your views here.
 
-def IndexView(self):
-    return HttpResponse("Hello, this is a to-do list.")
+class IndexView(generic.ListView):
+    template_name = 'todo/index.html'
+    context_object_name = 'note_list'
 
-def TodayView(self):
-    return HttpResponse("Hello this is to-do list of today.")
+    def get_queryset(self):
+        Notes = Note.objects.all()
+        NotExpiredNotes = [note.id for note in Notes if note.isExpired() != True]
+        return Note.objects.filter(id__in=NotExpiredNotes).order_by('todoDate')
 
-def WeekView(self):
-    return HttpResponse("Hello this is to-do list of this week.")
+class TodayView(generic.ListView):
+    template_name = 'todo/index.html'
+    context_object_name = 'note_list'
+
+    def get_queryset(self):
+        Notes = Note.objects.all()
+        TodaysNotes = [note.id for note in Notes if note.isToday()]
+        return Note.objects.filter(id__in=TodaysNotes).order_by('todoDate')
+
+class WeekView(generic.ListView):
+    template_name = 'todo/index.html'
+    context_object_name = 'note_list'
+
+    def get_queryset(self):
+        Notes = Note.objects.all()
+        WeeksNotes = [note.id for note in Notes if note.isThisWeek()]
+        return Note.objects.filter(id__in=WeeksNotes).order_by('todoDate')
+
+
+class ExpiredView(generic.ListView):
+    template_name = 'todo/index.html'
+    context_object_name = 'note_list'
+
+    def get_queryset(self):
+        Notes = Note.objects.all()
+        ExpiredNotes = [note.id for note in Notes if note.isExpired()]
+        return Note.objects.filter(id__in=ExpiredNotes).order_by('todoDate')
 
 def AddItemView(self):
     return HttpResponse("Hello this is to add items to the to-do list.")
-
-def ExpiredView(self):
-    return HttpResponse("Hello this is expired items view.")
