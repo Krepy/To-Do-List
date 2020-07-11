4237e51c15fce366af6cff8290e4c4225599e254
Krepy <neadiatre@gmail.com>
2020-07-11 15:52:55 +0300

    added notedetailview and additemview urls

--- todolist/todo/urls.py
+++ todolist/todo/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = 'todo'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('today', views.TodayView.as_view(), name='today'),
-    path('week', views.WeekView.as_view(), name='week'),
-    path('expired', views.ExpiredView.as_view(), name='expired'),
-    path('add', views.AddItemView, name='add'),
+    path('today/', views.TodayView.as_view(), name='today'),
+    path('week/', views.WeekView.as_view(), name='week'),
+    path('expired/', views.ExpiredView.as_view(), name='expired'),
+    path('add/', views.AddItemView.as_view(), name='add'),
+    path('note/<int:pk>/', views.NoteDetailView.as_view(), name='detail'),
 ]
