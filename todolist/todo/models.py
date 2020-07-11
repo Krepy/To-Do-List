6c71c668d00bc63499b88f55f219042f4f4e6a11
Krepy <neadiatre@gmail.com>
2020-07-11 15:53:57 +0300

    corrections

--- todolist/todo/models.py
+++ todolist/todo/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 import datetime
 
 # Create your models here.
 class Note(models.Model):
-    noteText = models.CharField(max_length=200)
-    todoDate = models.DateField('todo date')
+    noteTitle = models.CharField('Note Title', max_length=100, default = "Placeholder")
+    noteText = models.CharField('Note Text', max_length=500)
+    todoDate = models.DateField('To-Do at')
 
     def __str__(self):
         return self.noteText
@@ -16,3 +18,6 @@ class Note(models.Model):
         return timezone.now().date() <= self.todoDate <= timezone.now().date() + datetime.timedelta(days = 6)
     def isExpired(self):
         return self.todoDate < timezone.now().date()
+
+    def get_absolute_url(self):
+        return reverse("todo:detail", kwargs={"pk": self.id})
